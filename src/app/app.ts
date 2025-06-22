74877ce2c93f292bc4d1e4834f5b5b559e9b2acf
joel-jcs <joeljcs@protonmail.com>
2025-06-22 19:12:00 -0400

    chore: install prettier and tailwind; other initial settings and chores

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core'
+import { RouterOutlet } from '@angular/router'
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+    selector: 'app-root',
+    imports: [RouterOutlet],
+    templateUrl: './app.html',
+    styleUrl: './app.css',
 })
 export class App {
-  protected title = 'angular-crash';
+    protected title: string = 'Task Tracker'
 }
